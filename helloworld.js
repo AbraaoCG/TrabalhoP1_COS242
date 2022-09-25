a4cbcff9dbd1055ef605a268edc1b27fcc5c5e64
unknown <giovanni.maffeo@prismacapital.com>
2022-09-25 10:16:26 -0300

    first commit

---
+++ helloworld.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+function adjacency_matrix() {
+    try {
+        const data = fs.readFileSync('input.txt', 'utf8');
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+adjacency_matrix()
+
