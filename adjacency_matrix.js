a55864ca74dba12435f675e2ec2d1bccd7d21405
Abraao_PC <abraao.cg@yahoo.com>
2022-09-25 18:12:31 -0300

    Adição de classe mãe Graph, e relação com classe matriz de adjacência

--- adjacency_matrix.js
+++ adjacency_matrix.js
@@ -1,13 +1,38 @@
 const fs = require('fs');
+class Graph{ // Classe Base para Grafos
+    constructor(Input_txt_data,Graph_Type){
+        switch (Graph_Type) {
+            case 0: // Matrix de adj.
+                let Graph_Object = new Adjacency_matrix(Input_txt_data)
+                //console.log(x.n)
+                this.Graph_Struct = Graph_Object.Matrix
+                this.n = Graph_Object.n
+                this.m = Graph_Object.m
+                
+                console.log(this.Graph_Struct,this.n, this.m)
+                break;
+            case 1: // Lista de adj.
 
-class Adjacency_matrix {
+                break
+            case 2: // Vetor de adj.
+              
+                break;
+            default:
+              console.log("O armazenamento do Grafo deve ser feito em : \n1-Matriz de adjacências.\n2-Lista de adjacências.\n3-Vetor de adjacências");
+          }
+          
+    }
+}
+
+class Adjacency_matrix { //Classe para incializar matriz de ajacencia
     constructor(input) {
         //this.n, this.m, this.matrix = this.read_input(txt);
+        this.Assembly_Matrix(input)
     }
 
-    Assembly_Matrix(txt) {
+    Assembly_Matrix(input) {
         try {
-            let data = fs.readFileSync('input.txt', 'utf8');
+            let data = fs.readFileSync(input, 'utf8');
             
             // Definir num. de vértices.
             this.n = parseInt(data[0])
@@ -34,16 +59,23 @@ class Adjacency_matrix {
                 Matrix[v1][v2] = 1 // Seto v1 --> v2
                 Matrix[v2][v1] = 1 // Seto v2 --> v1
             }
-            console.log(Matrix)
+            this.Matrix = Matrix
 
         } catch (err) {
             console.error(err);
         }
+
     }
+
+    // Precisamos adicionar um método para obter graus mínimo, máximo, media e mediana de grau.
+}
+class Adjacency_List{
+
 }
 
-const myArray = new Adjacency_matrix("aa");
-myArray.Assembly_Matrix();
+
+let x  = new Graph("input.txt",0)
+
 
 
 
