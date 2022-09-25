50e1e088fc348517ec2218f692e03dc10ea1b527
unknown <giovanni.maffeo@prismacapital.com>
2022-09-25 11:09:56 -0300

    iniciando adjacency_matrix

--- adjacency_matrix.js
+++ adjacency_matrix.js
@@ -1,5 +1,30 @@
 const fs = require('fs');
 
+class Adjacency_matrix {
+    constructor(input) {
+        //this.n, this.m, this.matrix = this.read_input(txt);
+    }
+
+    read_file(txt) {
+        try {
+            const data = fs.readFileSync('input.txt', 'utf8');
+            console.log(type(data));
+            
+            nv = int(data[0])
+            console.log(nv)
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+const myArray = new Adjacency_matrix("aa");
+myArray.read_file();
+
+
+/*const fs = require('fs');
+
 function adjacency_matrix() {
     try {
         const data = fs.readFileSync('input.txt', 'utf8');
@@ -9,5 +34,5 @@ function adjacency_matrix() {
     }
 }
 
-adjacency_matrix()
+adjacency_matrix()*/
 
