060af2017dd49aa2c27c3fd0f61f5427fc275bc2
unknown <giovanni.maffeo@prismacapital.com>
2022-10-02 13:50:23 -0300

    inicializando datastruct

---
+++ linkedList.js
@@ -0,0 +1,32 @@
+class LinkedList {
+    constructor(v0 = null) {
+        this.head = v0 === null ? null : new ListNode(v0)
+        this.last = this.head
+        this.size = v0 === null ? 0 : 1
+    }
+
+    append(v){
+        if (this.head == null) {
+            this.head = new ListNode(v);
+        } else {
+            this.last.next = new ListNode(v) 
+            this.last = this.last.next 
+            this.size++
+        }
+    }
+}
+
+class ListNode { // Classe no para lista encadeada
+    constructor(data) {
+        this.data = data
+        this.next = null                
+    }
+}
+
+let list = new LinkedList()
+list.append(1)
+list.append(2)
+console.log(list)
+
+module.exports = LinkedList; // Export class
+
