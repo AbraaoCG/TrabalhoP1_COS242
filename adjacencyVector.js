0ac0a9a5d28fabc89fa11f4b090afc923ff54730
Abraao_PC <abraao.cg@yahoo.com>
2022-10-03 19:45:52 -0300

    Pull da main com adição da Bfs para Matrizes. Implementação da bfs para listas e vetores realizada

--- adjacencyVector.js
+++ adjacencyVector.js
@@ -5,9 +5,37 @@ class AdjacencyVector extends GraphStruct { // Classe Base para Grafos
         super(inputPath, graphType);
     } 
 
-    /*bfs(s) {
-        
-    }*/
+    bfs(s) {
+        // Desmarcar todos os vértices
+        let markupVector = new Array(this.n);
+        for (let i = 0 ; i < this.n ; i++){
+            markupVector[i] = 0;
+        }
+
+        // Definir fila Q vazia --> Precisa ser com fila???: https://stackoverflow.com/questions/1590247/how-do-you-implement-a-stack-and-a-queue-in-javascript?page=1&tab=scoredesc#tab-top
+        let q = [];
+        // Marcar s e inserir s na fila Q
+        markupVector[s - 1] = 1; // Subtraimos 1 porque o índice do vetor começa em zero
+        q.push(s - 1); // Subtraimos 1 porque o índice dos vértices na matriz começa em zero
+
+        while (q.length !== 0) { // Enquanto Q não estiver vazia
+            let v = q.shift();
+            console.log(v);
+
+            // Para percorrer os vizinhos, basta percorrer a lista de adjacência de cada vertice v
+            
+            for(let i = 0; i < this.struct[v].length; i++){
+                let w = this.struct[v][i]
+                // Preciso verificar se esse vizinho não foi marcado (Lembrando que na lista de adjacência implementada o nó 1 é guardado como 1, e não como 0. )
+                if (markupVector[w - 1] === 0) { // Se w não estiver marcado
+                    markupVector[w - 1] = 1;
+                    q.push(w - 1);
+                }
+            }     
+        }
+            
+        return q;
+    }
 }
 
 
