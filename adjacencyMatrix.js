29818fa4df03de876418694327de228827a49e18
unknown <giovanni.maffeo@prismacapital.com>
2022-10-03 21:44:43 -0300

    spanning tree done

--- adjacencyMatrix.js
+++ adjacencyMatrix.js
@@ -6,35 +6,39 @@ class AdjacencyMatrix extends GraphStruct { // Classe Base para Grafos
     } 
 
     bfs(s) {
-        // Desmarcar todos os vértices
+        this.writeOutput(['--- Ouput BFS ---'])
+        // Desmarcar todos os vértices com o valor de -1. Isso porque o markupVector receberá o nível de cada vértice
         let markupVector = new Array(this.n);
         for (let i = 0 ; i < this.n ; i++){
-            markupVector[i] = 0;
+            markupVector[i] = -1;
         }
 
-        // Definir fila Q vazia --> Precisa ser com fila???: https://stackoverflow.com/questions/1590247/how-do-you-implement-a-stack-and-a-queue-in-javascript?page=1&tab=scoredesc#tab-top
+        // Definir fila Q vazia
         let q = [];
         // Marcar s e inserir s na fila Q
-        markupVector[s - 1] = 1; // Subtraimos 1 porque o índice do vetor começa em zero
+        markupVector[s - 1] = 0; // Subtraimos 1 porque o índice do vetor começa em zero
         q.push(s - 1); // Subtraimos 1 porque o índice dos vértices na matriz começa em zero
 
+        this.writeOutput([`Nível ${markupVector[s - 1]}: `, `Vértice ${s} (raíz)`]) // Imprime o nó raíz
+        
         while (q.length !== 0) { // Enquanto Q não estiver vazia
-            let v = q.shift();
-            console.log(v);
+            let v = q.shift(); // Retirar v de Q
             
             // Para percorrer os vizinhos, fazemos assim? Poderia ter um uma estrutura (vetor) com cada vizinho do vértice (pegar o grau como comprimento desse vetor)
             // Está percorrendo a linha inteira da matriz
             for (let w = 0 ; w < this.n ; w++){
                 if (this.struct[v][w] === 1) {
-                    if (markupVector[w] === 0) { // Se w não estiver marcado
-                        markupVector[w] = 1;
+                    if (markupVector[w] === -1) { // Se w não estiver marcado
+                        markupVector[w] = markupVector[v] + 1; // O nó "w" que é filho do nó "v", terá 1 nível a mais que "v"
                         q.push(w);
+
+                        // Imprime o nó e o pai do nó descoberto (será aquele que o encontrou, isto é, o vértice "v")
+                        // Adicionamos 1 aos vértices porque o índice dos vértices na matriz começa em zero
+                        this.writeOutput([`Nível ${markupVector[w]}: `, `Vértice ${w + 1} (pai: ${v + 1})`]) 
                     }
-                }
+                } 
             }
         }
-
-        return q;
     }
 }
 
