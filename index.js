d49ef2f0a5921997da2d519b14d93c51db989084
unknown <giovanni.maffeo@prismacapital.com>
2022-11-02 13:22:21 -0300

    read input and weightAdjacencyList done

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-const AdjacencyMatrix = require('./adjacencyMatrix');
-const AdjacencyList = require('./adjacencyList');
-const AdjacencyVector = require('./adjacencyVector');
-const input = 'grafo_3.txt'
+const WeightAdjacencyList = require('./weightAdjacencyList');
+const LinkedList = require('./linkedList');
+const input = 'input.txt'
 
-myGraph = new AdjacencyVector(input);
-
-const start = Date.now()
-
-for (let k = 1; k <= 1000; k++) {
-    myGraph.bfs(k)         
-}
-
-const executionTime = (Date.now() - start) / 1000
-
-
-console.log(executionTime)
-
-//sleep(5000)
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+myGraph = new WeightAdjacencyList(input);
