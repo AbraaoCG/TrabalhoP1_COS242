ac04bfd55bf267ba702b8d88d7c1beca66f8afa7
unknown <giovanni.maffeo@prismacapital.com>
2022-11-08 21:58:59 -0300

    revisao e comentarios adicionados

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const Heap = require('heap-js')
 const WeightAdjacencyList = require('./weightAdjacencyList');
 const LinkedList = require('./linkedList');
 const input = 'input.txt';
 
 myGraph = new WeightAdjacencyList(input);
-console.log(myGraph.prim(1));
+dist = myGraph.mst();
--- weightAdjacencyList.js
+++ weightAdjacencyList.js
@@ -29,7 +29,7 @@ class WeightAdjacencyList extends WeightGraph { // Classe Base para Grafos
         let min = Infinity;
         let minIndex;
         for (let i = 0 ; i < this.n ; i++) {
-            if (exploitedArray[i] === false && baseArray[i] < min) {
+            if (exploitedArray[i] === false && baseArray[i] <= min) {
                 minIndex = i;
                 min = baseArray[minIndex];
             };
@@ -56,6 +56,9 @@ class WeightAdjacencyList extends WeightGraph { // Classe Base para Grafos
         let u;
         for (let i = 0 ; i < this.n ; i++) {
             u = this.getMin(exploitedArray, dist);
+
+            if (dist[u] === Infinity) { break }; // Quando o mínimo é um vértice com distância infinito quer dizer que a componente conexa acabou (conjunto dos descobertos acabou)
+
             exploitedArray[u] = true;
 
             let v = this.struct[u].head;
@@ -97,7 +100,7 @@ class WeightAdjacencyList extends WeightGraph { // Classe Base para Grafos
 
         let u; // Vértice de maior prioridade
         let dist_u; // Distância da origem de maior prioridade
-        for (let i = 0 ; i < this.n ; i++) { // Itera-se em 'n', pois sabemos que todos os vértices serão selecionados como 'u' para serem explorados.
+        while (distHeap[0][0] !== Infinity) { // No momento um vértice com distância infinita está na raiz, significa que a componente conexa terminou de ser analisada (conjunto descobertos acabou)
             [dist_u, u] = distHeap.extractMin(); // Extrai a menor distância na Heap, reordenando a Heap para manter a heap-order e também reordena o array heapIndex
             dist[u] = dist_u;  // Após explorado, a distância de "s" à "u" não será alterada novamente
             
@@ -108,8 +111,10 @@ class WeightAdjacencyList extends WeightGraph { // Classe Base para Grafos
                 if (dist[v.data[0]] === Infinity){ // Se ainda não foi explorado, descubro vizinho
                     vIndex = v.data[0];
                     edgeWeight = v.data[1];  
-                    heapIndexToChange = distHeap.heapIndex[vIndex];
-                    //console.log(distHeap[heapIndexToChange][0] , dist_u + edgeWeight)
+                    heapIndexToChange = distHeap.heapIndex[vIndex]; // índice na Heap referente ao vértice "v"
+                    if (vIndex === 47615) {
+                        console.log(vIndex);
+                    };
                     if (distHeap[heapIndexToChange][0] > (dist_u + edgeWeight)) {
                         distHeap.changePriority(heapIndexToChange, dist_u + edgeWeight);
                         parent[vIndex] = u;
@@ -122,75 +127,69 @@ class WeightAdjacencyList extends WeightGraph { // Classe Base para Grafos
         return [dist, parent];
     };
 
-    getMinimalPath(s, parent) {
-        let minimalPath = [s + 1];
-        let v = s;
-        while(parent[v] !== undefined) { // Ainda não chegou até a raíz
-            minimalPath.unshift(parent[v] + 1);
+    getMinimalPath(u, parent) { // Encontra caminho mínimo do vértice "u" até a "s" utilizando o array de parents
+        let minimalPath = [u + 1]; // Inicia uma lista de caminho mínimo com o vértice "u"
+        let v = u;
+        while(parent[v] !== undefined) { // Itera sobre o array de parent até chegar em "undefined", isto é, na raiz
+            minimalPath.unshift(parent[v] + 1); // Adiciona o pai de "v" no ínicio da lista de caminho mínimo
             v = parent[v];
         };
 
         return minimalPath;
     };
 
-    distAndMinimalPathHeap(s, v = null) {
-        let [dist, parent] = this.dijkstraHeap(s);
+    distAndMinimalPathHeap(s) {
+        let [dist, parent] = this.dijkstraHeap(s); // Retorna array de distâncias até "s" e o array de pais usando dijkstra com Heap
         let minimalPath;
-        for (let i = 0 ; i < dist.length ; i++) {
-            minimalPath = this.getMinimalPath(i, parent);
-            //console.log(`Distância do vértice ${s} até ${i + 1} é: ${dist[i]}. Um dos caminhos mínimos é dado por ${minimalPath}`);
+        for (let i = 0 ; i < dist.length ; i++) { // Itera sobre todos os vértices do grafo imprimindo a distância e o caminho mínimo de "i" até "s"
+            minimalPath = this.getMinimalPath(i, parent); // Calcula caminho mínimo de "i" até "s"
+            this.writeOutput(`Distância do vértice ${s} até ${i + 1} é: ${dist[i]}. Um dos caminhos mínimos é dado por ${minimalPath}`);
         };
-        if (v != null) return dist[ v - 1 ]
-        else{ return dist };
+        return dist;
     };
 
-    distAndMinimalPathVector(s, v = null) {
-        let [dist, parent] = this.dijkstraVector(s);
+    distAndMinimalPathVector(s) {
+        let [dist, parent] = this.dijkstraVector(s); // Retorna array de distâncias até "s" e o array de pais usando dijkstra sem Heap
         let minimalPath;
-        for (let i = 0 ; i < dist.length ; i++) {
-            minimalPath = this.getMinimalPath(i, parent);
-            //console.log(`Distância do vértice ${s} até ${i + 1} é: ${dist[i]}. Um dos caminhos mínimos é dado por ${minimalPath}`);
+        for (let i = 0 ; i < dist.length ; i++) { // Itera sobre todos os vértices do grafo imprimindo a distância e o caminho mínimo de "i" até "s"
+            minimalPath = this.getMinimalPath(i, parent); // Calcula caminho mínimo de "i" até "s"
+            this.writeOutput(`Distância do vértice ${s} até ${i + 1} é: ${dist[i]}. Um dos caminhos mínimos é dado por ${minimalPath}`);
         };
-        if (v != null) return dist[ v - 1 ]
-        else{ return dist };
+        return dist;
     };
 
     prim(s) {
-        let costHeap = new Heap(this.n, true);
-        let cost = new Array(this.n);
-        let parent = new Array(this.n);
-        let exploitedArray = new Array(this.n);
+        let costHeap = new Heap(this.n, true); // Ordena os custos em uma Heap
+        let cost = new Array(this.n); // Armazena o custo de cada vértice
+        let parent = new Array(this.n); // Armazena o pai de cada vértice da mst
         for (let i = 0 ; i < this.n ; i++){
-            cost[i] = Infinity
+            cost[i] = Infinity;
             parent[i] = undefined;
-            exploitedArray[i] = false;
-            costHeap.insert([Infinity, i])
+            costHeap.insert([Infinity, i]);
             costHeap.heapIndex[i] = i;
         };
 
-        costHeap.changePriority(costHeap.heapIndex[s - 1], 0);
+        costHeap.changePriority(costHeap.heapIndex[s - 1], 0); // Adiciona o custo da raiz como zero na Heap
 
         let u;
         let cost_u;
-        for (let i = 0 ; i < this.n ; i++) {
-            [cost_u, u] = costHeap.extractMin();
-            cost[u] = cost_u;
-            exploitedArray[u] = true;
+        while (costHeap[0][0] !== Infinity) {
+            [cost_u, u] = costHeap.extractMin(); // Extrai a custo e o vértice raiz da Heap, ou seja, aquela que tem menor custo dentre as descobertas
+            cost[u] = cost_u; // Define o custo definitivo do vértice "u" que será explorado
 
-            let v = this.struct[u].head;
+            let v = this.struct[u].head; // Descobre o primeiro vizinho de u
             let vIndex;
             let edgeWeight;
             let heapIndexToChange;
             while(v != null){
                 vIndex = v.data[0];
-                // Verificamos se o vértice "v" vizinho de "u" já foi explorado, se ainda não foi, talvez teremos que atualizar seu custo
-                if (cost[v.data[0]] === Infinity) { 
+                if (cost[v.data[0]] === Infinity) { // Verificamos se o vértice "v" vizinho de "u" já foi explorado, se já foi, o seu custo não será mais atualizado
                     edgeWeight = v.data[1];  
-                    heapIndexToChange = costHeap.heapIndex[vIndex];
-                    // Se custo da aresta w(u,v) para adicioná-lo é maior do que o custo de "v" e o vértice "v" ainda não foi explorado, atualizamos seu custo
-                    if (costHeap[heapIndexToChange][0] > edgeWeight) {
-                        costHeap.changePriority(heapIndexToChange, edgeWeight);
-                        parent[vIndex] = u;
+                    heapIndexToChange = costHeap.heapIndex[vIndex]; // índice na Heap referente ao vértice "v"
+                    // Se custo da aresta w(u,v) para adicioná-lo é maior do que o custo de "v", atualizamos seu custo
+                    if (costHeap[heapIndexToChange][0] > edgeWeight) { 
+                        costHeap.changePriority(heapIndexToChange, edgeWeight); // Atualiza custo de "v" na Heap
+                        parent[vIndex] = u; // Atualiza "u" como pai de "v" no array de pais
                     };    
                 };
                 v = v.next;
@@ -201,15 +200,18 @@ class WeightAdjacencyList extends WeightGraph { // Classe Base para Grafos
     };
 
     mst() {
+        // Método para imprimir árvore geradora mínima (mst), que deve ser escrita em um arquivo (mesmo formato que um grafo), assim como seu peso total
         this.writeOutput(['--- Ouput MST ---']);
-        const [cost, parent] = this.prim(1);
+        const [cost, parent] = this.prim(1); // Inicia o algoritmo de prim no vértice 1 (poderia iniciar por qualquer vértice para um grafo conexo)
         let total = 0;
-        for (let i = 1 ; i < cost.length ; i++) {
-            total += cost[i];
-            this.writeOutput([`${parent[i] + 1} ${i + 1}`]);
+        for (let i = 0 ; i < cost.length ; i++) { // Pula o vértice raiz porque não tem pai 
+            if (cost[i] !== Infinity && cost[i] !== 0) { // Não vai imprimir vértices com custo infinito (não estão na mesma componente conexa), nem a raiz (não tem pai)
+                total += cost[i]; // Soma o custo total da mst
+                this.writeOutput([`${parent[i] + 1} ${i + 1} ${cost[i]}`]); // Imprime aresta da seguinte forma: pai de "i", "i", custo de "i"
+            };
         };
         this.writeOutput([`Custo total: ${total}`]);
-    }
+    };
 };
 
 module.exports = WeightAdjacencyList; // Export class
