b58fce9c80ca503f28a43ad2644ba4ff7229e78e
unknown <giovanni.maffeo@prismacapital.com>
2022-12-11 20:14:53 -0300

    ford fulkerson completo

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-const WeightAdjacencyList = require('./weightAdjacencyList');
-const LinkedList = require('./linkedList');
-const input = 'grafo3_w.txt'; // endereco do grafo a ser analisado
+const weightDirectedAdjacencyList = require('./weightDirectedAdjacencyList');
+const input = 'grafo_rf_5.txt'; // endereco do grafo a ser analisado
 
-myGraph = new WeightAdjacencyList(input);
+myGraph = new weightDirectedAdjacencyList(input);
 
-
-// Todos os testes estão comentados entre /**/. Para realizar um dos testes requitados, retire dos comentários.
-
-
-// Teste Mst
-/**/
-time_i = Date.now()
-//myGraph.mst(1);
-myGraph.mstNoOutput(1); // ---> mais rapida, apenas retorna custo total.
-tdif = ( Date.now() - time_i ) / 1000
-console.log(" tempo medio de execucao para obter mst : ", tdif , " s")
-
-
-
-
-// Teste de tempo médio para calcular distâncias comecando de ˋnumˋ  vertices aleatórios.
-/*
-let n = myGraph.n
-let s = 0;
-num = 1
+writeOutput = false;
+let [s,t] = [1, 2];
+num = 1;
 time_i = Date.now()
 for(x = 0; x < num ; x++) {
-    s = Math.floor( Math.random() * n ) + 1 // Math random  = [0,1[
-    s = 1
-    //console.log(s)
-    myGraph.dijkstraHeap(s);
-    //myGraph.dijkstraVector(s);
+    console.log("fluxo:", myGraph.fordFulkersonWithDelta(s, t, writeOutput)[0])
 }
 
 tTotal = ( Date.now() - time_i ) / 1000
 tMedio = tTotal / num
-console.log(" tempo medio de execucao de ", num ," Dijkstraˋs : ", tMedio , " s")
-*/
-
-
-// Teste de distância s à v, escolhidos pelo usuário.
-/*
-s = 10 
-v = 40
-dist_sv = myGraph.dijkstraHeap(s)[0]
-//dist_sv = myGraph.distAndMinimalPathHeap(s) // Tempo de execução para escrever em output.txt as distâncias e caminhos mínimos consideravelmente maior.
-console.log(dist_sv[v - 1])
-
-*/
+console.log(" tempo medio de execucao de ", num ," Ford Fulkerson's : ", tMedio , " s")
